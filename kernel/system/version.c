16a9c8fc8ff5a75fe943d0c1bcda5482160664c7
Kross1de <awesome.hubygu@yandex.ru>
2025-04-22 16:26:27 +0600

    heap

---
+++ kernel/system/version.c
@@ -0,0 +1,9 @@
+const char *__kernel_name = "netwide";
+
+int __kernel_version_major = 1;
+int __kernel_version_minor = 2;
+
+const char *__kernel_arch = "x86_64";
+
+const char *__kernel_build_date = __DATE__;
+const char *__kernel_build_time = __TIME__;

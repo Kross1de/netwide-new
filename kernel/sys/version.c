d759d85fed9004ff70ff6aa8e2a55984d68d2598
Kross1de <awesome.hubygu@yandex.ru>
2025-05-01 10:26:52 +0600

    Delete kernel directory

--- kernel/sys/version.c
+++
@@ -1,7 +0,0 @@
-const char *__kernel_name = "netwide";
-
-int __kernel_version_major = 1;
-int __kernel_version_minor = 2;
-
-const char *__kernel_build_date = __DATE__;
-const char *__kernel_build_time = __TIME__;
